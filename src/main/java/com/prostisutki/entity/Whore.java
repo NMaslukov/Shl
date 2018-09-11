4a22afd5f9638c379e4883028d29f26497f5bf3e
NMaslukov <mikita.malykov@upholding.biz>
2018-09-11 17:13:59 +0300

    whore

---
+++ src/main/java/com/prostisutki/entity/Whore.java
@@ -0,0 +1,20 @@
+package com.prostisutki.entity;
+
+import com.prostisutki.resources.annotations.Column;
+import com.prostisutki.resources.annotations.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "whores")
+public class Whore {
+
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+}
